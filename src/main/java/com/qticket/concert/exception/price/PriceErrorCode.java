9820f2ff9f24ad2eecd5912da48efcc0dbc3e70e
Gido <120196095+gidopa@users.noreply.github.com>
2024-10-03 19:15:15 +0900

    feat: 가격 응답 dto / ErrorCode

---
+++ src/main/java/com/qticket/concert/exception/price/PriceErrorCode.java
@@ -0,0 +1,32 @@
+package com.qticket.concert.exception.price;
+
+import com.qticket.concert.exceptionCommon.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum PriceErrorCode implements ErrorCode {
+  NOT_FOUND(HttpStatus.NOT_FOUND, "PRICE_001", "가격이 존재하지 않습니다"),
+  ;
+
+  private final HttpStatus status;
+  private final String code;
+  private final String message;
+
+  @Override
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  @Override
+  public String getCode() {
+    return code;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+}
